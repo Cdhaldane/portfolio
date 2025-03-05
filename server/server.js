97a9e583f051bc1c6447dec1c848e27f01b9d66b
Charlie Haldane <48765995+Cdhaldane@users.noreply.github.com>
2025-03-05 16:38:00 -0500

    r

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const userRoutes = require("./routes/user.routes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Enable CORS
 app.use(cors());
--- src/App.js
+++ src/App.js
@@ -64,7 +64,9 @@ const App = () => {
     // Fetch users when the component mounts
     async function fetchUsers() {
       try {
-        const response = await fetch("http://localhost:5000/api/users");
+        const response = await fetch(
+          process.env.REACT_APP_BACKEND_URL + "/api/users"
+        );
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
